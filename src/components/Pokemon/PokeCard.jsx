812a05bfe0914b74ba67670134610c50cccb680a
cyblogerz <pranavajay2002@gmail.com>
2023-09-18 22:43:23 +0530

    Fixed the transition

--- src/components/Pokemon/PokeCard.jsx
+++ src/components/Pokemon/PokeCard.jsx
@@ -2,26 +2,25 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const PokeCard = (props) => {
-    const [isHovered, setIsHovered] = useState(false);
+//     const [isHovered, setIsHovered] = useState(false);
 
-const handleMouseEnter = () => {
-  setIsHovered(true);
-};
+// const handleMouseEnter = () => {
+//   setIsHovered(true);
+// };
 
-const handleMouseLeave = () => {
-  setIsHovered(false);
-};
-const cardStyles = {
-    transform: isHovered ? 'scale(1.1)' : 'scale(1)',
-    transition: 'transform 0.3s ease', 
-  };
+// const handleMouseLeave = () => {
+//   setIsHovered(false);
+// };
+// const cardStyles = {
+//     transform: isHovered ? 'scale(1.1)' : 'scale(1)',
+//     transition: 'transform 0.3s ease', 
+//   };
 
     const title = props.pokemonName;
     const imageUrl = props.pokemonImgUrl;
   return (
     
-    <div className="bg-white shadow-md rounded-lg p-4 "   style={cardStyles} onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}>
+    <div className=" transition ease-in-out delay-50  hover:scale-110  bg-white shadow-md rounded-lg p-4 ">
       <Link to={`/pokemon/${title}`}>
        <h2 className="text-xl font-semibold mb-2">{title}</h2>
        <div className="flex justify-center ptop-2">
