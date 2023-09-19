c2b399446188cb23b2d5ffac7558373470cded10
cyblogerz <pranavajay2002@gmail.com>
2023-09-19 15:47:50 +0530

    Added animation

--- src/components/Pokemon/PokeDetailCard.jsx
+++ src/components/Pokemon/PokeDetailCard.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { StatsBar } from '../UI/Stats';
+import NumAnim from '../UI/numAnim';
 
 const PokeDetailCard = (props) => {
     const findStat = (statName) => props.stats.find((stat) => stat.stat.name === statName)?.base_stat;
@@ -16,22 +18,28 @@ const PokeDetailCard = (props) => {
     <div className='flex md:flex-row     pb-4'>
         <div className='pr-4'>
             <p className='text-sm'>Height</p>
-            <p className='text-3xl font-bold'>{props.height} m</p>
+            <NumAnim value={props.height} met={'m'}/> 
         </div>
         <div>
             <p className='text-sm'>Weight</p>
-            <p className='text-3xl font-bold'>{props.weight} kg</p>
+            <NumAnim value={props.weight} met={'kg'}/> 
         </div>
         
     </div>
 
-    <ul>
+    <ul className='pb-10'>
         <li>HP: {findStat('hp')}</li>
+        <StatsBar value={findStat('hp')}/>
         <li>Attack: {findStat('attack')}</li>
+        <StatsBar value={findStat('attack')}/>
         <li>Defense: {findStat('defense')}</li>
+        <StatsBar value={findStat('defense')}/>
         <li>Special Attack: {findStat('special-attack')}</li>
+        <StatsBar value={findStat('special-attack')}/>
         <li>Special Defense: {findStat('special-defense')}</li>
+        <StatsBar value={findStat('special-defense')}/>
         <li>Speed: {findStat('speed')}</li>
+        <StatsBar value={findStat('speed')}/>
       </ul>
 
 
