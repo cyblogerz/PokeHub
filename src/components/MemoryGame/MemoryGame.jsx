51796029207aa25528ca7e0ec1a2462e8a0eb1ce
cyblogerz <pranavajay2002@gmail.com>
2023-09-26 12:47:27 +0530

    added wallpaper

--- src/components/MemoryGame/MemoryGame.jsx
+++ src/components/MemoryGame/MemoryGame.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const MemoryGame = () => {
+    
   return (
-    <div>MemoryGame</div>
+    <div className="bg-[url(pokemon-bg.png)] bg-cover h-screen pt-20 flex flex-col justify-center items-center">
+    <h1 className="text-xl mb-10 ">Score : 0 &nbsp; High Score : 🏆5</h1>
+<div className="grid grid-cols-4  md:flex md:flex-row gap-4 h-full">
+  <div>01</div>
+  <div>02</div>
+  <div>03</div>
+  <div>04</div>
+  <div>05</div>
+  <div>06</div>
+  
+</div>
+</div>
   )
 }
 
