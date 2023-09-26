d92e274f8be08613288f5740835a0099b7c5d5ba
cyblogerz <pranavajay2002@gmail.com>
2023-09-26 11:12:24 +0530

    updated search bar

--- src/components/UI/SearchBar.jsx
+++ src/components/UI/SearchBar.jsx
@@ -7,7 +7,7 @@ function SearchBar() {
       <input
         type="text"
         className="bg-white border border-gray-300 rounded-md pl-10 py-2 w-full focus:outline-none focus:ring focus:border-blue-500"
-        placeholder="Search Pokemon..."
+        placeholder="Search..."
       />
       <div className="absolute inset-y-0 left-0 flex items-center pl-3">
         <AiOutlineSearch className="text-gray-400" />
